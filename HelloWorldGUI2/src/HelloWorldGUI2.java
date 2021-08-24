a8a3fac33f13bdc1163ac9de1b47ab83f9ea832b
Trua Shamu <48630514+TruaShamu@users.noreply.github.com>
2021-08-24 09:24:36 -0500

    Add files via upload

---
+++ HelloWorldGUI2/src/HelloWorldGUI2.java
@@ -0,0 +1,41 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class HelloWorldGUI2 {
+
+    private static class HelloWorldDisplay extends JPanel {
+        public void paintComponent(Graphics g) {
+            super.paintComponent(g);
+            g.drawString("Hello World!", 20, 30);
+        }
+    }
+
+    private static class ButtonHandler implements ActionListener {
+        public void actionPerformed(ActionEvent e) {
+            System.exit(0);
+        }
+    }
+
+    public static void main(String[] args) {
+
+        HelloWorldDisplay displayPanel = new HelloWorldDisplay();
+        JButton okButton = new JButton("OK");
+        ButtonHandler listener = new ButtonHandler();
+        okButton.addActionListener(listener);
+
+        JPanel content = new JPanel();
+        content.setLayout(new BorderLayout());
+        content.add(displayPanel, BorderLayout.CENTER);
+        content.add(okButton, BorderLayout.SOUTH);
+
+        JFrame window = new JFrame("GUI Test");
+        window.setContentPane(content);
+        window.setSize(250, 100);
+        window.setLocation(100, 100);
+        window.setVisible(true);
+
+    }
+
+}
